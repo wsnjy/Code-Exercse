64c15b9c34eb0b90d92316daf1fcd7b98d790bd4
Wisnu Sanjaya <wisnu@sebangsa.com>
2018-08-14 16:52:49 +0700

    Sock Merchant

---
+++ Sock Merchant.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+// Problem https://www.hackerrank.com/challenges/sock-merchant/problem
+
+func sockMerchant(n: Int, ar: [Int]) -> Int {
+    
+    var socks = [Int:Int]()
+    var socksCount = 0
+    
+    for number in ar {
+        
+        if (socks[number] != nil) {
+            socks[number] = socks[number]! + 1
+        }else{
+            socks[number] = 1
+        }
+    }
+    
+    for (key,value) in socks {
+        if value < 2 {
+            socks.removeValue(forKey: key)
+        }
+    }
+    
+    for item in socks {
+        socksCount += item.value/2
+    }
+    
+    return socksCount
+}
+
+sockMerchant(n: 7, ar: [1,2,1,2,1,3,2])
